f5a95ecf0077037af495c8027a32937fa2968277
Luke <38190809+belikeluke@users.noreply.github.com>
2018-04-11 21:49:56 -0400

    Add files via upload

--- src/Container.java
+++ src/Container.java
@@ -6,6 +6,8 @@ public class Container {
 		Scanner input = new Scanner(System.in);
 		input.useDelimiter("\\D"); //Allows you to input the regular IP format instead of each octet individually
 		int oct[] = new int[4];
+      int nHosts = 0;
+      int nSubnets = 0;
 		
 		System.out.println("Enter the ip address (. excluded)");
 		
@@ -13,9 +15,19 @@ public class Container {
 		for(int i = 0; i < 4; i++) {
 			oct[i] = input.nextInt();
 		}
-		input.close();
+      
+      input.close();
+      
+      Scanner second = new Scanner(System.in);
+      System.out.println("Enter Hosts Needed: ");
+      nHosts = second.nextInt();
+      
+      System.out.println("Enter Subnets Needed: ");
+      nSubnets = second.nextInt();
+      
+      second.close();
 		
-		
+      
 		//Printing IP
 		System.out.println("Network Address:");
 		for(int i = 0; i < 4; i++) {
@@ -38,6 +50,19 @@ public class Container {
 		for(int i = 0; i < 4; i++) {
 			System.out.print(defaultMaskArray[i] + ".");
 		}
+      
+      System.out.println("\n");
+      
+      //Printing Given / Needed Subnets and Hosts
+      NandG gan = new NandG(nHosts, nSubnets);
+      System.out.println("Needed Hosts:");
+      System.out.println(nHosts);
+      System.out.println("Usable Hosts:");
+      System.out.println((gan.getGHosts() -2));
+      
+      System.out.println("Needed Subnets:");
+      System.out.println(nSubnets);
+      System.out.println("Usable Subnets:");
 		
 		//Exporting data	
 		FileWriter writing = new FileWriter("test.csv");
